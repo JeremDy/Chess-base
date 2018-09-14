c50c67910912af473aa61637a7777f178afd085f
axel-kirk <mackirk.dev@gmail.com>
2018-09-14 10:39:07 +0200

    mp chat OK, comment JS public & delete redux add chrome

--- public/javascript/socket.js
+++ public/javascript/socket.js
@@ -1,63 +1,63 @@
-var app = {
-/*eslint:disable*/
-    init : function(){
+// var app = {
+// /*eslint:disable*/
+//     init : function(){
 
-        $('#chat').on('submit', app.sendMessage);
-        $('#match-making').on('click', app.matchMaking);
+//         $('#chat').on('submit', app.sendMessage);
+//         $('#match-making').on('click', app.matchMaking);
 
-        console.log('init');
+//         console.log('init');
 
-        var webSocket = WS.connect("ws://127.0.0.1:8080");
+//         var webSocket = WS.connect("ws://127.0.0.1:8080");
 
-        webSocket.on("socket/connect", function(session){
-            app.session = session;
-            session.subscribe("chat/global", function(uri, payload){
-                console.log('payload sub chat global',payload);
-            });
+//         webSocket.on("socket/connect", function(session){
+//             app.session = session;
+//             session.subscribe("chat/global", function(uri, payload){
+//                 console.log('payload sub chat global',payload);
+//             });
 
 
-            session.subscribe("chat/private", function(uri, payload){		
-                console.log('payload sub chat private',payload);		
+//             session.subscribe("chat/private", function(uri, payload){		
+//                 console.log('payload sub chat private',payload);		
        
-            });
+//             });
 
-        })
+//         })
      
 
-        webSocket.on("socket/disconnect", function(error){
-            //error provides us with some insight into the disconnection: error.reason and error.code
+//         webSocket.on("socket/disconnect", function(error){
+//             //error provides us with some insight into the disconnection: error.reason and error.code
 
-            console.log("Disconnected for " + error.reason + " with code " + error.code);
-        })
-    },
+//             console.log("Disconnected for " + error.reason + " with code " + error.code);
+//         })
+//     },
 
-    sendMessage :function(evt){
-        evt.preventDefault();
+//     sendMessage :function(evt){
+//         evt.preventDefault();
        
-        var message = $(this).find('input').val();
-        var receiver =$(this).find('select').val();
+//         var message = $(this).find('input').val();
+//         var receiver =$(this).find('select').val();
         
-        if(receiver === 'public'){
-            app.session.publish("chat/global", { 'receiver': receiver , 'message' : message});
-        }
-        else{
-            app.session.publish("chat/private", { 'receiver': receiver , 'message' : message});
-        }
-    },
+//         if(receiver === 'public'){
+//             app.session.publish("chat/global", { 'receiver': receiver , 'message' : message});
+//         }
+//         else{
+//             app.session.publish("chat/private", { 'receiver': receiver , 'message' : message});
+//         }
+//     },
 
     
-    matchMaking : function(evt){
-        /*console.log(app.session);*/
-        app.session.subscribe('matchmaking', function(uri,payload){
-            console.log(payload);
-            if(typeof payload.matchFound !== 'undefined'){
-                app.session.unsubscribe('matchmaking');
-            }
-        });
+//     matchMaking : function(evt){
+//         /*console.log(app.session);*/
+//         app.session.subscribe('matchmaking', function(uri,payload){
+//             console.log(payload);
+//             if(typeof payload.matchFound !== 'undefined'){
+//                 app.session.unsubscribe('matchmaking');
+//             }
+//         });
 
 
-    }
+//     }
 
-}
+// }
 
-$(app.init);
+// $(app.init);
