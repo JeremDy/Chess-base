d89743749cabf68d0a4c1f014ef62562230652ca
Max'-C <maxime.cohier@gmail.com>
2018-09-26 11:24:32 +0200

    Suppression Redis

--- config/bundles.php
+++ config/bundles.php
@@ -22,6 +22,5 @@ return [
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
